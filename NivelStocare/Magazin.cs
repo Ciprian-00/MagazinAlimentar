5e033d5a6e2999d8f32707ad1afe5cd2b65dae88
Ciprian Ciobanu <ciprian.ciobanu@student.usv.ro>
2025-03-10 12:40:34 +0200

    Add files via upload

---
+++ NivelStocare/Magazin.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System;
+using MagazinAlimentar.Modele;
+using System.Collections.Generic;
+
+namespace MagazinAlimentar.NivelStocare
+{
+    public class Magazin
+    {
+        public string Nume { get; set; }
+        public List<Produs> Produse { get; set; }
+
+        public Magazin(string nume)
+        {
+            Nume = nume;
+            Produse = new List<Produs>();
+        }
+
+        public void AdaugaProdus(Produs produs)
+        {
+            Produse.Add(produs);
+        }
+
+        public void AfiseazaProduse()
+        {
+            Console.WriteLine($"Produse din magazinul {Nume}:");
+            foreach (var produs in Produse)
+            {
+                produs.AfiseazaProdus();
+            }
+        }
+
+        public Produs CautaProdusDupaNume(string nume)
+        {
+            return Produse.FirstOrDefault(p => p.Nume.Equals(nume, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
