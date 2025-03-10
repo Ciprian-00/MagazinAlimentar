7385788bb8c56e668e86d3efb6ce46fac445e1c6
Ciprian Ciobanu <ciprian.ciobanu@student.usv.ro>
2025-03-10 12:40:15 +0200

    Add files via upload

---
+++ Modele/Produs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MagazinAlimentar.Modele
+{
+    public class Produs
+    {
+        // Proprietăți auto-implemented
+        public string Nume { get; set; }
+        public double Pret { get; set; }
+        public int Stoc { get; set; }
+
+        public Produs(string nume, double pret, int stoc)
+        {
+            Nume = nume;
+            Pret = pret;
+            Stoc = stoc;
+        }
+
+        public void AfiseazaProdus()
+        {
+            Console.WriteLine($"Produs: {Nume}, Pret: {Pret} RON, Stoc: {Stoc} buc.");
+        }
+    }
+}
